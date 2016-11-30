bd18b34f0c56f07d8208f27724f665105c3a3fd4
Renan Lukas <renan.lukas@youse.com.br>
2016-11-30 09:01:59 -0200

    Check if it is problematic device when choosing Camera version.

--- library/src/main/java/com/google/android/cameraview/CameraView.java
+++ library/src/main/java/com/google/android/cameraview/CameraView.java
@@ -96,7 +96,7 @@ public class CameraView extends FrameLayout {
             preview = new TextureViewPreview(context, this);
         }
         mCallbacks = new CallbackBridge();
-        if (Build.VERSION.SDK_INT < 21) {
+        if (Build.VERSION.SDK_INT < 21 || isProblematicDeviceOnNewCameraApi()) {
             mImpl = new Camera1(mCallbacks, preview);
         } else if (Build.VERSION.SDK_INT < 23) {
             mImpl = new Camera2(mCallbacks, preview, context);
@@ -508,4 +508,47 @@ public class CameraView extends FrameLayout {
         }
     }
 
+    private boolean isProblematicDeviceOnNewCameraApi() {
+        if ("Huawei".equals(Build.MANUFACTURER) &&
+                "angler".equals(Build.PRODUCT)) {
+            return true;
+        } else if ("LGE".equals(Build.MANUFACTURER) &&
+                "occam".equals(Build.PRODUCT)) {
+            return true;
+        } else if ("Amazon".equals(Build.MANUFACTURER) &&
+                "full_ford".equals(Build.PRODUCT)) {
+            return true;
+        } else if ("Amazon".equals(Build.MANUFACTURER) &&
+                "full_thebes".equals(Build.PRODUCT)) {
+            return true;
+        } else if ("HTC".equals(Build.MANUFACTURER) &&
+                "m7_google".equals(Build.PRODUCT)) {
+            return true;
+        } else if ("HTC".equals(Build.MANUFACTURER) &&
+                "hiaeuhl_00709".equals(Build.PRODUCT)) {
+            return true;
+        } else if ("Wileyfox".equals(Build.MANUFACTURER) &&
+                "Swift".equals(Build.PRODUCT)) {
+            return true;
+        } else if ("Sony".equals(Build.MANUFACTURER) &&
+                "C6603".equals(Build.PRODUCT)) {
+            return true;
+        } else if ("Sony".equals(Build.MANUFACTURER) &&
+                "C6802".equals(Build.PRODUCT)) {
+            return true;
+        } else if ("samsung".equals(Build.MANUFACTURER) &&
+                "zerofltexx".equals(Build.PRODUCT)) {
+            return true;
+        } else if ("OnePlus".equals(Build.MANUFACTURER) &&
+                Build.MODEL.startsWith("ONE E100")) {
+            return true;
+        } else if ("LGE".equals(Build.MANUFACTURER) &&
+                Build.MODEL.equals("LGUS991")) {
+            return true;
+        } else if ("LGE".equals(Build.MANUFACTURER) &&
+                Build.MODEL.equals("LG-H815")) {
+            return true;
+        }
+        return false;
+    }
 }
