e4a76457b073f4564da33b72b6ec00d5d96f40e4
Renan Lukas <renan.lukas@youse.com.br>
2016-12-01 16:31:48 -0200

    Code review - extracting variables.

--- library/src/main/java/com/google/android/cameraview/CameraView.java
+++ library/src/main/java/com/google/android/cameraview/CameraView.java
@@ -100,7 +100,9 @@ public class CameraView extends FrameLayout {
         }
         mCallbacks = new CallbackBridge();
 
-        if(new CameraSettings().shouldUseCamera2((CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE))) {
+        final CameraManager cameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
+        final CameraSettings cameraSettings = new CameraSettings();
+        if(cameraSettings.shouldUseCamera2(cameraManager)) {
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
                 mImpl = new Camera2(mCallbacks, preview, context);
             } else {
